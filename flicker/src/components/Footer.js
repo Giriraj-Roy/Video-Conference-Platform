5534b4ddb62944287a32b52fca53e7c7ba14170f
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-08 10:33:49 +0530

    Initial setup

---
+++ flicker/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='footer-main'>
+            This is Footer
+        </div>
+    )
+}
+
+export default Footer
