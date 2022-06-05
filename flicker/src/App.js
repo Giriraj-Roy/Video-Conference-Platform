bd6f4100272f8a36aac7b95e3233837ac8c27512
Giriraj Roy <girirajroy2001@gmail.com>
2022-06-05 10:46:42 +0530

    Updating styles

--- flicker/src/App.js
+++ flicker/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
 
 const App = () => {
   return (
     <div>
-      This is App Body
+      {/* This is App Body */}
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
     </div>
   )
 }
--- flicker/src/components/Footer.js
+++ flicker/src/components/Footer.js
@@ -3,7 +3,14 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='footer-main'>
+            <hr/>
             This is Footer
+            <div>
+                About
+            </div>
+            <div>
+                Contact
+            </div>
         </div>
     )
 }
